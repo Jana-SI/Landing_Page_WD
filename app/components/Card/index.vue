9667ddb386abf84accaaecee92de5d14ca500b59
Jana-SI <janaina.adolfo.ds@gmail.com>
2025-10-05 19:23:27 -0300

    ajustes card e css

--- app/components/Card/index.vue
+++ app/components/Card/index.vue
@@ -2,10 +2,15 @@
     <div class="card">
         <div class="card-img-top retangulo"></div>
         <div class="card-body">
-            <h4 class="titulo-card">Lorem ipsum dolor sit amet?</h4>
-            <p class="text-card">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse augue sem, molestie non maximus non,
-                fermentum quis urna. In porta condimentum odio non varius. Nam id euismod augue, sit amet placerat quam.
-            </p>
+            <h4 class="titulo-card">{{ title }}</h4>
+            <p class="text-card">{{ description }}</p>
         </div>
     </div>
 </template>
+
+<script setup>
+defineProps({
+    title: String,
+    description: String
+})
+</script>
--- app/components/PageContainer/index.vue
+++ app/components/PageContainer/index.vue
@@ -1,25 +1,27 @@
 <template>
-    <section class="py-5">
+    <section>
         <div class="container-fluid text-center">
             <div class="row">
                 <div class="col-md-6 offset-md-3">
                     <h2 class="pre-titulo">Texto aqui</h2>
                     <h1 class="titulo">Mais outro bem texto aqui</h1>
                     <p class="texto">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mattis risus sit amet libero porttitor laoreet. Nullam ut volutpat nisi. Phasellus id tempus orci. Donec eget orci eget dolor condimentum sodales.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mattis risus sit amet libero
+                        porttitor laoreet. Nullam ut volutpat nisi. Phasellus id tempus orci. Donec eget orci eget dolor
+                        condimentum sodales.
                     </p>
                 </div>
             </div>
 
             <div class="row">
-                <div class="col-md-10 offset-md-1">
+                <div class="d-flex justify-content-center">
                     <div id="cardCarousel" class="carousel slide" data-bs-ride="carousel">
                         <div class="carousel-inner">
                             <div v-for="(group, index) in groupedCards" :key="index"
                                 :class="['carousel-item', index === 0 ? 'active' : '']">
-                                <div class="row">
-                                    <div v-for="card in group" :key="card" class="col-12 col-md-6 col-lg-3 card-column">
-                                        <Card />
+                                <div class="row g-3 justify-content-center">
+                                    <div v-for="card in group" :key="card" class="card-column">
+                                        <Card :title="card.title" :description="card.description" />
                                     </div>
                                 </div>
                             </div>
@@ -53,46 +55,49 @@
 import { ref, onMounted } from 'vue'
 
 const totalCards = 12
-const cards = Array.from({ length: totalCards }, (_, i) => i + 1)
+const cards = Array.from({ length: totalCards }, () => ({
+  title: 'Lorem ipsum dolor sit amet?',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse augue sem, molestie non maximus non, fermentum quis urna. In porta condimentum odio non varius. Nam id euismod augue, sit amet placerat quam.'
+}))
 const groupedCards = ref([])
 
 const activeIndex = ref(0)
 let bsCarousel = null
 
 function groupCards() {
-  const isMobile = window.innerWidth < 768
-  const groupSize = isMobile ? 1 : 4
+    const isMobile = window.innerWidth < 768
+    const groupSize = isMobile ? 2 : 4
 
-  groupedCards.value = []
-  for (let i = 0; i < cards.length; i += groupSize) {
-    groupedCards.value.push(cards.slice(i, i + groupSize))
-  }
+    groupedCards.value = []
+    for (let i = 0; i < cards.length; i += groupSize) {
+        groupedCards.value.push(cards.slice(i, i + groupSize))
+    }
 }
 
 function goToSlide(index) {
-  if (bsCarousel) {
-    bsCarousel.to(index)
-  }
+    if (bsCarousel) {
+        bsCarousel.to(index)
+    }
 }
 
 onMounted(() => {
-  groupCards()
-
-  const carouselEl = document.getElementById('cardCarousel')
-  if (carouselEl) {
-    bsCarousel = new bootstrap.Carousel(carouselEl, {
-      interval: false,
-      ride: false,
-    })
-
-    carouselEl.addEventListener('slid.bs.carousel', (event) => {
-      activeIndex.value = event.to
-    })
-  }
-
-  // Atualiza o agrupamento se a tela for redimensionada
-  window.addEventListener('resize', () => {
     groupCards()
-  })
+
+    const carouselEl = document.getElementById('cardCarousel')
+    if (carouselEl) {
+        bsCarousel = new bootstrap.Carousel(carouselEl, {
+            interval: false,
+            ride: false,
+        })
+
+        carouselEl.addEventListener('slid.bs.carousel', (event) => {
+            activeIndex.value = event.to
+        })
+    }
+
+    // Atualiza o agrupamento se a tela for redimensionada
+    window.addEventListener('resize', () => {
+        groupCards()
+    })
 })
 </script>
